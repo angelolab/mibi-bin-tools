5b5c3827cfabb6cd152c585d64dd16ccaea96294
alex-l-kong <alkong@ucdavis.edu>
2023-10-19 16:29:37 -0700

    Fix test for get_total_spectra

--- tests/bin_files_test.py
+++ tests/bin_files_test.py
@@ -343,7 +343,7 @@ def test_get_total_counts(test_dir, fov):
         bytes(bf, 'utf-8'), np.array([0], np.uint16),
         np.array([-1], dtype=np.uint16), np.array([False], dtype=np.uint8)
     )
-    assert(total_counts['fov-1-scan-1'] == np.sum(total_ion_image[0, :, :, :]))
+    assert total_counts['fov-1-scan-1'] == np.sum(total_ion_image[0, :, :, :])
 
 
 @parametrize_with_cases('test_dir, fov', cases=FovMetadataTestFiles)
@@ -351,6 +351,6 @@ def test_get_total_counts(test_dir, fov):
 def test_get_total_spectra(test_dir, fov, panel):
     bin_files.get_total_spectra(
         test_dir,
-        fov['json'].split('.')[0],
+        [fov['json'].split('.')[0]],
         panel
     )
